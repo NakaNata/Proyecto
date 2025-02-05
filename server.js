afe758992f1e4562d5beec2ef0c71a48d288171b
Natalia Gutierrez Puerta <nataliagutierrezpuerta@gmail.com>
2025-02-05 21:08:54 +0100

    Prettier

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-"use strict";
-import dotenv from "dotenv";
+'use strict';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const { PORT } = process.env;
 
 // algo
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get('/', function (req, res) {
+    res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-  console.log(`http://127.0.0.1:${PORT}`);
+    console.log(`http://127.0.0.1:${PORT}`);
 });
